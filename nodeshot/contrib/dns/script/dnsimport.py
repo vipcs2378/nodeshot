c70b0f76d1b4eaa9cb57dd64fffd5b50e5bc8284
root <root@DS-Server.ninux.org>
2012-08-08 23:32:37 +0200

    Added domainize function

--- nodeshot/contrib/dns/script/dnsimport.py
+++ nodeshot/contrib/dns/script/dnsimport.py
@@ -1,9 +1,14 @@
+from re import sub
+
 class DnsImport:
 
-    def domainize(self, *args, **kwargs)
+    def domainize(self, name, *args, **kwargs)
     """
-    Method who convert a string in a domain compatible name, just containing a-z and -
+    Method who convert a string in a domain compatible name, just containing a-z and _
     """
-        return True
+	dn = name.lower()
+	dn = sub(r'[^a-z0-9]+', '_', dn).strip('_')
+
+        return dn
 
 
