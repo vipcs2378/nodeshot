2ae399a3a3a53f5303fa7b68bb2082866c83343d
Dario Salerno <terribilywrong@gmail.com>
2012-12-19 19:42:04 +0100

    Implemented domainize function

--- nodeshot/contrib/dns/script.py
+++ nodeshot/contrib/dns/script.py
@@ -1,12 +1,12 @@
 from re import sub
 
-def domainize(self, name, *args, **kwargs)
+def domainize(name)
 """
 Method who convert a string in a domain compatible name
 """
     domainized_name = name.lower()
     domainized_name = sub(r'[^a-z0-9]+', '_', dn).strip('_')
 
-    return dn
+    return domainized_name
 
 
